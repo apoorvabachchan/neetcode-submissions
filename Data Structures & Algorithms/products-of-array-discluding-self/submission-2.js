1ad6de8360fb97e0a46287078f812db2a4f789f4
Apoorva Bachchan <apoorvabachchan@gmail.com>
2026-07-20 17:04:31 +0100

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let res = []
+        let start = 1
+        for (let i = 0; i < nums.length; i++ ) {
+            res.push(start)
+            start = start * nums[i]
+        }
+        let start2 = 1
+        for (let i = nums.length -1; i >= 0; i--) {
+            res[i] = start2 * res[i]
+            start2 = start2 * nums[i]
+        }
+        return res
+    }
+}
